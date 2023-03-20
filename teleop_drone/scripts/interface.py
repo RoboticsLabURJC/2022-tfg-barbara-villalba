224ebc67433cd36c3f6ba194fbf320aeae192cb9
bb6698 <b.villalba.2019@alumnos.urjc.es>
2023-03-20 19:30:07 +0100

    Changed node teleop and reorganised code, node interface change orientation for each controls

--- teleop_drone/scripts/interface.py
+++ teleop_drone/scripts/interface.py
@@ -1,11 +1,8 @@
 import rospy
 from PyQt5.QtWidgets import *
 from geometry_msgs.msg import PoseStamped,Twist
-import sys
-import message_filters
-from sensor_msgs.msg import CameraInfo, Image
-from cv_bridge import CvBridge, CvBridgeError
-import numpy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge 
 import time
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
@@ -13,17 +10,26 @@ from PyQt5 import QtGui
 import cv2
 from std_msgs.msg import String
 
-MAX_HEIGHT = 200
-MAX_WIDTH = 800
+MAX_HEIGHT = 100
+MAX_WIDTH = 700
 
+MAX_POSITION_X = 5
+MIN_POSITION_X = -5
+
+MAX_ANGLE_YAW = 180
+MIN_ANGLE_YAW = 0
+
+MAX_VELOCITY_X = 5
+MIN_VELOCITY_X = 0
+
+MAX_SECONDS = 1
+
+# Topics
 IMAGE_TOPIC = '/iris/usb_cam/image_raw'
-
 MODE_TOPIC = '/commands/mode'
-
 POSITION_TOPIC = '/commands/control_position'
-VALUE_POSITION_TOPIC = '/commands/control_position/value'
-
 VELOCITY_TOPIC = '/commands/control_velocity'
+
 
 class ImageCapture():
     def __init__(self,interface):
@@ -32,17 +38,30 @@ class ImageCapture():
         self.image_label = interface.label
         self.bridge = CvBridge()
         self.lastTime = time.time()
+        self.counter_time = 0.0
+        self.isfirst = False 
+        self.fps = 0
         self.list = []
-
-    def Calculate_fps(self):
+        
+    def calculate_fps(self):
         fps = 1/(time.time() - self.lastTime)
         self.list.append(fps)
         return sum(self.list)/len(self.list)
-    
+
     def callback_image(self,image):
         rgb_image =  self.bridge .imgmsg_to_cv2(image, desired_encoding="rgb8")
-        fps = self.Calculate_fps()
-        cv2.putText(rgb_image, "FPS:" + str(int(fps)), (40, 40),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        self.counter_time += (time.time() - self.lastTime)
+        if(not self.isfirst):
+            self.fps = self.calculate_fps()
+            self.isfirst = True
+
+        #--Update each 1 seconds
+        if(self.counter_time > MAX_SECONDS):
+            self.fps = self.calculate_fps()
+            self.counter_time = 0.0
+
+        cv2.putText(rgb_image, "FPS:" + str(int(self.fps)), (40, 40),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QtGui.QImage(
@@ -56,28 +75,17 @@ class Interface():
         self.app = QApplication([])
         self.window = QWidget()
         self.layout = QGridLayout()
-        self.window.setWindowTitle('Interfaz')
+        self.window.setWindowTitle('Teleop Drone')
         self.label = QLabel("Camera View")
         self.window.resize(MAX_WIDTH,MAX_HEIGHT)
         self.pub_mode = rospy.Publisher(MODE_TOPIC, String, queue_size=10)
-        self.pub_position = rospy.Publisher(POSITION_TOPIC, String, queue_size=10)
         self.pub_velocity = rospy.Publisher(VELOCITY_TOPIC, Twist, queue_size=10)
-        self.pub_value_position = rospy.Publisher(VALUE_POSITION_TOPIC, PoseStamped, queue_size=10)
-        self.pub_position.publish('')
+        self.pub_position = rospy.Publisher(POSITION_TOPIC, PoseStamped, queue_size=10)
         self.msg_v = Twist()
         self.msg_position = PoseStamped()
 
     def addImages(self):
         self.layout.addWidget(self.label,0,0)
-        
-
-    def addTitle(self):
-        self.label_title_position = QLabel("Control position")
-        self.layout.addWidget(self.label_title_position,1,1)
-
-        self.label_title_velocity = QLabel("Control velocity")
-        self.layout.addWidget(self.label_title_velocity,1,3)
-
 
     def addButtons(self):
         self.btn_land = QPushButton(self.window)
@@ -90,130 +98,104 @@ class Interface():
         self.btn_take_off.clicked.connect(self.take_off_clicked)
         self.layout.addWidget(self.btn_take_off,0,3)
 
-        
-        self.btn_row_position = QPushButton(self.window)
-        self.btn_row_position.setText('ROW')
-        self.btn_row_position.clicked.connect(self.row_position)
-        self.layout.addWidget(self.btn_row_position, 4, 1)
+        self.btn_control_position = QPushButton(self.window)
+        self.btn_control_position.setText('POSITION')
+        self.btn_control_position.clicked.connect(self.position_clicked)
+        self.layout.addWidget(self.btn_control_position,1,1)
 
-        self.btn_pitch_position = QPushButton(self.window)
-        self.btn_pitch_position.setText('PITCH')
-        self.btn_pitch_position.clicked.connect(self.pitch_position)
-        self.layout.addWidget(self.btn_pitch_position, 5, 1)
+        self.btn_control_velocity = QPushButton(self.window)
+        self.btn_control_velocity.setText('VELOCITY')
+        self.btn_control_velocity.clicked.connect(self.velocity_clicked)
+        self.layout.addWidget(self.btn_control_velocity,1,3)
 
-        self.btn_yaw_position = QPushButton(self.window)
-        self.btn_yaw_position.setText('YAW')
-        self.btn_yaw_position.clicked.connect(self.yaw_position)
-        self.layout.addWidget(self.btn_yaw_position, 6, 1)
-
-       
     def land_clicked(self):
         self.pub_mode.publish('LAND') 
 
     def take_off_clicked(self):
         self.pub_mode.publish('TAKE OFF') 
 
-    def row_position(self):
-        self.pub_position.publish('ROW')
+    def position_clicked(self):
+        self.pub_mode.publish('POSITION') 
 
-    def pitch_position(self):
-        self.pub_position.publish('PITCH')
+    def velocity_clicked(self):
+        self.pub_mode.publish('VELOCITY') 
 
-    def yaw_position(self):
-        self.pub_position.publish('YAW')
+    def take_off_clicked(self):
+        self.pub_mode.publish('TAKE OFF') 
 
     def comanderPosition(self):
         self.ValuePosition.setText( "Position in exe x : " + str(self.PositionSlider.value()))
-        self.layout.addWidget(self.ValuePosition,7,1)
+        self.layout.addWidget(self.ValuePosition,5,1)
         self.msg_position.pose.position.x = self.PositionSlider.value()
-        self.pub_value_position.publish(self.msg_position)
+        self.pub_position.publish(self.msg_position)
+
+    def comanderAngleYawPosition(self):
+        self.ValueYawPosition.setText( "Yaw: " + str(self.YawPositionSlider.value()) + " degrees")
+        self.layout.addWidget(self.ValueYawPosition,2,1)
+        self.msg_position.pose.orientation.z = self.YawPositionSlider.value()
+        self.pub_position.publish(self.msg_position)
 
     def comanderVelocity(self):
         
         self.ValueVelocity.setText( "Velocity in exe x : " + str(self.VelocitySlider.value()))
-        self.layout.addWidget(self.ValueVelocity,4,4)
+        self.layout.addWidget(self.ValueVelocity,5,3)
         self.msg_v.linear.x = self.VelocitySlider.value()
         self.pub_velocity.publish(self.msg_v)
 
-    def comanderVelocityRawX(self):
-        self.ValueRawX.setText( "Row : " + str(self.Raw_xSlider.value()))
-        self.layout.addWidget(self.ValueRawX,5,4)
-        self.msg_v.angular.x = self.Raw_xSlider.value()
-        self.pub_velocity.publish(self.msg_v)
-
-    def comanderVelocityRawY(self):
-        self.ValueRawY.setText( "Pitch : " + str(self.Raw_ySlider.value()))
-        self.layout.addWidget(self.ValueRawY,6,4)
-        self.msg_v.angular.y = self.Raw_ySlider.value()
-        self.pub_velocity.publish(self.msg_v)
 
     def comanderVelocityRawZ(self):
-        self.ValueRawZ.setText( "Yaw : " + str(self.Raw_zSlider.value()))
-        self.layout.addWidget(self.ValueRawZ,7,4)
+        self.ValueRawZ.setText( "Yaw : " + str(self.Raw_zSlider.value()) + " degrees")
+        self.layout.addWidget(self.ValueRawZ,2,3)
         self.msg_v.angular.z = self.Raw_zSlider.value()
         self.pub_velocity.publish(self.msg_v)
 
     def addSliders(self):
         self.PositionSlider = QSlider(QtCore.Qt.Horizontal)
-        self.PositionSlider.setMinimum(-5)
-        self.PositionSlider.setMaximum(5)
+        self.PositionSlider.setMinimum(MIN_POSITION_X)
+        self.PositionSlider.setMaximum(MAX_POSITION_X)
         self.PositionSlider.setValue(0)
         self.PositionSlider.setTickPosition(QSlider.TicksBelow)
         self.PositionSlider.setTickInterval(5)
-        self.layout.addWidget(self.PositionSlider,8,1)
+        self.layout.addWidget(self.PositionSlider,6,1)
         self.ValuePosition = QLabel("Value")
         self.ValuePosition.setText( "Position in exe x : " + str(self.PositionSlider.value()))
-        self.layout.addWidget(self.ValuePosition,7,1)
+        self.layout.addWidget(self.ValuePosition,5,1)
         self.PositionSlider.valueChanged.connect(self.comanderPosition)
+
+        self.YawPositionSlider = QSlider(QtCore.Qt.Horizontal)
+        self.YawPositionSlider.setMinimum(MIN_ANGLE_YAW)
+        self.YawPositionSlider.setMaximum(MAX_ANGLE_YAW)
+        self.YawPositionSlider.setValue(0)
+        self.YawPositionSlider.setTickPosition(QSlider.TicksBelow)
+        self.YawPositionSlider.setTickInterval(5)
+        self.layout.addWidget(self.YawPositionSlider,4,1)
+        self.ValueYawPosition = QLabel("Value")
+        self.ValueYawPosition.setText( "Yaw: " + str(self.YawPositionSlider.value()) + " degrees")
+        self.layout.addWidget(self.ValueYawPosition,2,1)
+        self.YawPositionSlider.valueChanged.connect(self.comanderAngleYawPosition)
         
         self.VelocitySlider = QSlider(QtCore.Qt.Horizontal)
-        self.VelocitySlider.setMinimum(0)
-        self.VelocitySlider.setMaximum(5)
+        self.VelocitySlider.setMinimum(MIN_VELOCITY_X)
+        self.VelocitySlider.setMaximum(MAX_VELOCITY_X)
         self.VelocitySlider.setValue(0)
         self.VelocitySlider.setTickPosition(QSlider.TicksBelow)
         self.VelocitySlider.setTickInterval(5)
-        self.layout.addWidget(self.VelocitySlider,4,3)
+        self.layout.addWidget(self.VelocitySlider,6,3)
         self.ValueVelocity = QLabel("Value")
         self.ValueVelocity.setText( "Velocity in exe x : " + str(self.VelocitySlider.value()))
-        self.layout.addWidget(self.ValueVelocity,4,4)
+        self.layout.addWidget(self.ValueVelocity,5,3)
         self.VelocitySlider.valueChanged.connect(self.comanderVelocity)
 
-        self.Raw_xSlider = QSlider(QtCore.Qt.Horizontal)
-        self.Raw_xSlider.setMinimum(0)
-        self.Raw_xSlider.setMaximum(5)
-        self.Raw_xSlider.setValue(0)
-        self.Raw_xSlider.setTickPosition(QSlider.TicksBelow)
-        self.Raw_xSlider.setTickInterval(5)
-        self.layout.addWidget(self.Raw_xSlider,5,3)
-        self.ValueRawX = QLabel("Value")
-        self.ValueRawX.setText( "Row : " + str(self.Raw_xSlider.value()))
-        self.layout.addWidget(self.ValueRawX,5,4)
-        self.Raw_xSlider.valueChanged.connect(self.comanderVelocityRawX)
-
-
-        self.Raw_ySlider = QSlider(QtCore.Qt.Horizontal)
-        self.Raw_ySlider.setMinimum(0)
-        self.Raw_ySlider.setMaximum(5)
-        self.Raw_ySlider.setValue(0)
-        self.Raw_ySlider.setTickPosition(QSlider.TicksBelow)
-        self.Raw_ySlider.setTickInterval(5)
-        self.layout.addWidget(self.Raw_ySlider,6,3)
-        self.ValueRawY = QLabel("Value")
-        self.ValueRawY.setText( "Pitch : " + str(self.Raw_ySlider.value()))
-        self.layout.addWidget(self.ValueRawY,6,4)
-        self.Raw_ySlider.valueChanged.connect(self.comanderVelocityRawY)
-
-
         self.Raw_zSlider = QSlider(QtCore.Qt.Horizontal)
-        self.Raw_zSlider.setMinimum(0)
-        self.Raw_zSlider.setMaximum(5)
+        self.Raw_zSlider.setMinimum(MIN_ANGLE_YAW)
+        self.Raw_zSlider.setMaximum(MAX_ANGLE_YAW)
         self.Raw_zSlider.setValue(0)
         self.Raw_zSlider.setTickPosition(QSlider.TicksBelow)
         self.Raw_zSlider.setTickInterval(5)
-        self.layout.addWidget(self.Raw_zSlider,7,3)
+        self.layout.addWidget(self.Raw_zSlider,4,3)
         self.ValueRawZ = QLabel("Value")
-        self.ValueRawZ.setText( "Yaw : " + str(self.Raw_zSlider.value()))
-        self.layout.addWidget(self.ValueRawZ,7,4)
+        self.ValueRawZ.setText( "Yaw : " + str(self.Raw_zSlider.value()) + " degrees")
+        self.layout.addWidget(self.ValueRawZ,2,3)
         self.Raw_zSlider.valueChanged.connect(self.comanderVelocityRawZ)
 
 
@@ -224,11 +206,9 @@ class Interface():
 
 rospy.init_node("interface_node_py")
 interface = Interface()
-interface.addTitle()
 interface.addButtons()
 interface.addImages()
 interface.addSliders()
-
 image = ImageCapture(interface)
 
 def main():
--- teleop_drone/scripts/teleop_node.py
+++ teleop_drone/scripts/teleop_node.py
@@ -1,88 +1,148 @@
-#! /usr/bin/env python
+
 import rospy
-from geometry_msgs.msg import PoseStamped
-from mavros_msgs.msg import State, PositionTarget
-from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from geometry_msgs.msg import PoseStamped, Twist
+from mavros_msgs.msg import State
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest, CommandTOL, CommandTOLRequest
 from std_msgs.msg import String
+import math
 
 # Topics
-LOCAL_POS_PUB = "mavros/setpoint_position/local"
+SETPOINT_POS_PUB = "/mavros/setpoint_position/local"
 STATE_SUB = "mavros/state"
+COMANDER_POSITION_SUB = '/commands/control_position'
+LOCAL_VEL_PUB = "/mavros/setpoint_velocity/cmd_vel_unstamped"
+COMANDER_VELOCITY_SUB = '/commands/control_velocity'
+LOCAL_POS__SUB = '/mavros/local_position/pose'
+MODE_SUB = "/commands/mode"
 
 # Services
 ARMING_CLIENT = "/mavros/cmd/arming"
 SET_MODE_CLIENT = "/mavros/set_mode"
+LAND_CMD_SRV = "/mavros/cmd/land"
+TAKE_OFF_CMD_CLIENT = "/mavros/cmd/takeoff"
 
+# Modes
 LAND = "LAND"
 TAKE_OFF = "TAKE OFF"
-ROW = "ROW"
-PITCH = "PITCH"
-YAW = "YAW"
+POSITION = "POSITION"
+VELOCITY = "VELOCITY"
 
 
-behaviour = " "
+MAX_HEIGH = 2
 
+mode = ""
+
+position = PoseStamped()
+value_position = PoseStamped()
+current_pos = PoseStamped()
+
+raw = Twist()
+value_velocity = Twist()
+istakeoff = False
 
 class Drone():
     def __init__(self):
         self.current_state = State()
         self.state_sub = rospy.Subscriber(STATE_SUB, State, callback=self.state_cb)
-        self.comander_sub = rospy.Subscriber('/interfaces/comander',
-                           String, self.manage_behavior)
-        self.local_pos_pub = rospy.Publisher(LOCAL_POS_PUB, PoseStamped, queue_size=10)
+        self.mode_sub = rospy.Subscriber(MODE_SUB, String, callback=self.control_mode_cb)
+        self.local_pos_pub = rospy.Publisher(SETPOINT_POS_PUB, PoseStamped, queue_size=10)
+        self.value_position_sub = rospy.Subscriber(COMANDER_POSITION_SUB, PoseStamped, self.get_value_position_cb)
+        self.value_velocity_sub = rospy.Subscriber(COMANDER_VELOCITY_SUB, Twist, self.get_value_velocity_cb)
+        self.local_raw_pub = rospy.Publisher(LOCAL_VEL_PUB, Twist, queue_size=10)
+        self.current_pos_sub = rospy.Subscriber(LOCAL_POS__SUB, PoseStamped, callback=self.current_pos_cb)
+        self.orientation_list = []
+        
 
     def state_cb(self, msg):
         self.current_state = msg
 
     def check_to_fly(self, offb_set_mode, arm_cmd, last_req, arming_client, set_mode_client):
 
+        '''
+        Check before to fly. The dron have a mode offboard and it's should armed before to fly
+        '''
+
         if (self.current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
-            if (set_mode_client.call(offb_set_mode).mode_sent == True):
+            if (set_mode_client.call(offb_set_mode).mode_sent is True):
                 rospy.loginfo("OFFBOARD enabled")
 
             last_req = rospy.Time.now()
+
         else:
             if (not self.current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
-                if (arming_client.call(arm_cmd).success == True):
+                if (arming_client.call(arm_cmd).success is True):
                     rospy.loginfo("Vehicle armed")
 
                 last_req = rospy.Time.now()
 
-    def manage_behavior(self, msg):
+    def current_pos_cb(self,pose):
 
-        global behaviour
-        behaviour = msg.data
+        '''
+        Callback, save current position for orientation in control position
+        '''
+        global current_pos
+        current_pos = pose
 
+    def control_mode_cb(self, msg):
+        '''
+        Callback, save mode: position,velocity,land or take off
+        '''
+        global mode
+        mode = msg.data
 
-    def check_behavior(self,pose):
+    def get_value_position_cb(self, msg):
 
-        if (behaviour == LAND):
-            pose.pose.position.x = 0
-            pose.pose.position.y = 0
-            pose.pose.position.z = 2
+        '''
+        Callback, save value for control position
+        '''
+        global value_position
+        value_position = msg
 
-        elif(behaviour == TAKE_OFF):
-            pose.pose.position.x = 0
-            pose.pose.position.y = 0
-            pose.pose.position.z = 0
+    def get_value_velocity_cb(self, msg):
 
-        elif(behaviour == ROW and pose.pose.position.z != 0):
-            pose.pose.orientation.x = 1
-            pose.pose.orientation.y = 0
-            pose.pose.orientation.z = 0
+        '''
+        Callback, save value for control velocity
+        '''
+        global value_velocity
+        value_velocity = msg
+    def control_position(self):
 
-        elif(behaviour == PITCH and pose.pose.position.z != 0):
-            pose.pose.orientation.x = 0
-            pose.pose.orientation.y = 1
-            pose.pose.orientation.z = 0
+        '''
+        Control position, position and orientation in radians in exe z (Yaw)
 
-        elif(behaviour == YAW and pose.pose.position.z != 0):
-            pose.pose.orientation.x = 0
-            pose.pose.orientation.y = 0
-            pose.pose.orientation.z = 1
+        Only works if the dron is not on the ground
+        '''
+        if (istakeoff):
+            
+            self.orientation_list = [current_pos.pose.orientation.x,
+                    current_pos.pose.orientation.y,
+                    current_pos.pose.orientation.z,
+                    current_pos.pose.orientation.w]
+            
+            roll, pitch, yaw = euler_from_quaternion(self.orientation_list)
+            
+            quaternion = quaternion_from_euler(roll, pitch, yaw + math.radians(value_position.pose.orientation.z))
 
+        
+            position.pose.orientation.x = quaternion[0]
+            position.pose.orientation.y = quaternion[1]
+            position.pose.orientation.z = quaternion[2]
+            position.pose.orientation.w = quaternion[3]
 
-        return pose
+            position.pose.position.x = value_position.pose.position.x
+
+    def control_velocity(self):
+        
+        '''
+        Control velocity, velocity lineal and angular in exe z
+
+        Only works if the dron is not on the ground
+        '''
+
+        if(istakeoff):
+            raw.linear.x = value_velocity.linear.x
+            raw.angular.z = math.radians(value_velocity.angular.z)
 
 
 if __name__ == "__main__":
@@ -95,7 +155,12 @@ if __name__ == "__main__":
     rospy.wait_for_service(SET_MODE_CLIENT)
     set_mode_client = rospy.ServiceProxy(SET_MODE_CLIENT, SetMode)
 
-    # Setpoint publishing MUST be faster than 2Hz
+    rospy.wait_for_service(LAND_CMD_SRV)
+    land_client = rospy.ServiceProxy(LAND_CMD_SRV, CommandTOL)
+
+    rospy.wait_for_service(TAKE_OFF_CMD_CLIENT)
+    take_off_client = rospy.ServiceProxy(TAKE_OFF_CMD_CLIENT, CommandTOL)
+
     rate = rospy.Rate(20)
 
     # Wait for Flight Controller connection
@@ -108,10 +173,12 @@ if __name__ == "__main__":
     arm_cmd = CommandBoolRequest()
     arm_cmd.value = True
 
-    pose = PoseStamped()
-    pose.pose.position.x = 0
-    pose.pose.position.y = 0
-    pose.pose.position.z = 0
+    land_cmd = CommandTOLRequest()
+    land_cmd.min_pitch = 0
+    land_cmd.yaw = 0
+    land_cmd.latitude = 0
+    land_cmd.longitude = 0
+    land_cmd.altitude = 0
 
     last_req = rospy.Time.now()
 
@@ -119,7 +186,22 @@ if __name__ == "__main__":
         drone.check_to_fly(offb_set_mode, arm_cmd, last_req,
                            arming_client, set_mode_client)
 
-        pose = drone.check_behavior(pose)
-        drone.local_pos_pub.publish(pose)
+        if(mode == TAKE_OFF):
+            position.pose.position.z = MAX_HEIGH
+            drone.local_pos_pub.publish(position)
+            istakeoff = True
 
+        elif (mode == POSITION):
+            drone.control_position()
+            drone.local_pos_pub.publish(position)
+
+        elif (mode == VELOCITY):
+            drone.control_velocity()
+            drone.local_raw_pub.publish(raw)
+
+        elif(mode == LAND):
+            if(rospy.Time.now() - last_req) > rospy.Duration(5.0):
+                response = land_client.call(land_cmd)
+                last_req = rospy.Time.now()
+        
         rate.sleep()
