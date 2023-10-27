1fbb1110dc0511e05fc67ea74b5cb8b66825c8a8
bb6698 <b.villalba.2019@alumnos.urjc.es>
2023-10-27 22:47:15 +0200

    Detection node with ONNX with GPU

--- src/percepcion/scripts/detection_yolop_onnx_node.py
+++ src/percepcion/scripts/detection_yolop_onnx_node.py
@@ -1,35 +1,34 @@
 #! /usr/bin/env python3
-
+import torch
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
 import cv2
-from torchvision import transforms
-import torch
 import time
 import onnxruntime as ort
+
 
 IMAGE_TOPIC = '/airsim_node/PX4/front_center_custom/Scene'
 
 ROUTE_MODEL = 'hustvl/yolop'
 MODEL = 'yolop'
 
-
 class ImageSubscriber:
     def __init__(self):
         self.bridge = CvBridge()
         self.image_pub = rospy.Publisher('/yolop/image',Image,queue_size=1)
         self.image_sub = rospy.Subscriber(IMAGE_TOPIC, Image, self.callback)
+        ort.set_default_logger_severity(4)
+        self.ort_session = ort.InferenceSession("/home/oem/YOLOP/weights/yolop-640-640.onnx",providers=['CUDAExecutionProvider'])
 
-    def resize_unscale(self,image,new_shape):
-
-        shape = image.shape[:2]  # current shape [height, width]
+    def resize_unscale(self,img, new_shape=(640, 640), color=114):
+        shape = img.shape[:2]  # current shape [height, width]
         if isinstance(new_shape, int):
             new_shape = (new_shape, new_shape)
 
         canvas = np.zeros((new_shape[0], new_shape[1], 3))
-        canvas.fill(114)
+        canvas.fill(color)
         # Scale ratio (new / old) new_shape(h,w)
         r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
 
@@ -43,19 +42,13 @@ class ImageSubscriber:
         dh = pad_h // 2
 
         if shape[::-1] != new_unpad:  # resize
-            image = cv2.resize(image, new_unpad, interpolation=cv2.INTER_AREA)
+            img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_AREA)
 
-        canvas[dh:dh + new_unpad_h, dw:dw + new_unpad_w, :] = image
+        canvas[dh:dh + new_unpad_h, dw:dw + new_unpad_w, :] = img
 
         return canvas, r, dw, dh, new_unpad_w, new_unpad_h  # (dw,dh)
     
     def infer_yolop(self,image):
-        height, width, _ = image.shape
-        ort_session = ort.InferenceSession("/home/oem/YOLOP/weights/yolop-640-640.onnx")
-
-        outputs_info = ort_session.get_outputs()
-        inputs_info = ort_session.get_inputs()
-
         canvas, r, dw, dh, new_unpad_w, new_unpad_h = self.resize_unscale(image, (640,640))
 
         img = canvas.copy().astype(np.float32)  # (3,640,640) RGB
@@ -71,11 +64,15 @@ class ImageSubscriber:
 
         img = np.expand_dims(img, 0)  # (1, 3,640,640)
 
+        t1 = time.time()
         # inference: (1,n,6) (1,2,640,640) (1,2,640,640)
-        _, da_seg_out, ll_seg_out = ort_session.run(
+        _, da_seg_out, ll_seg_out = self.ort_session.run(
             ['det_out', 'drive_area_seg', 'lane_line_seg'],
             input_feed={"images": img}
         )
+        t2 = time.time()
+
+        fps = 1 / (t2 - t1)
 
             # select da & ll segment area.
         da_seg_out = da_seg_out[:, :, dh:dh + new_unpad_h, dw:dw + new_unpad_w]
@@ -83,8 +80,8 @@ class ImageSubscriber:
 
         da_seg_mask = np.argmax(da_seg_out, axis=1)[0]  # (?,?) (0|1)
         ll_seg_mask = np.argmax(ll_seg_out, axis=1)[0]  # (?,?) (0|1)
-        print(da_seg_mask.shape)
-        print(ll_seg_mask.shape)
+        #--print(da_seg_mask.shape)
+        #--print(ll_seg_mask.shape)
 
         color_area = np.zeros((new_unpad_h, new_unpad_w, 3), dtype=np.uint8)
         color_area[da_seg_mask == 1] = [0, 255, 0]
@@ -93,32 +90,47 @@ class ImageSubscriber:
 
         # convert to BGR
         color_seg = color_seg[..., ::-1]
+        #--color_seg = cv2.resize(color_seg, (width, height))
         color_mask = np.mean(color_seg, 2)
-        img_merge = canvas[dh:dh + new_unpad_h, dw:dw + new_unpad_w, :]
-        img_merge = img_merge[:, :, ::-1]
+        img_merge = canvas
 
         # merge: resize to original size
         img_merge[color_mask != 0] = \
             img_merge[color_mask != 0] * 0.5 + color_seg[color_mask != 0] * 0.5
         img_merge = img_merge.astype(np.uint8)
-        img_merge = cv2.resize(img_merge, (width, height),
-                           interpolation=cv2.INTER_LINEAR)
-
-
-
+        
+        cv2.putText(
+            img_merge,
+            text=f"FPS: {fps:.1f}",
+            org=(15, 25),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1,
+            color=(0, 0, 255),
+            thickness=2,
+            lineType=cv2.LINE_AA
+        )
         return img_merge
-
 
 
     def callback(self, data):
 
+        t1 = time.time()
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8") 
 
         image_result = self.infer_yolop(cv_image)
-
-        image_message = self.bridge.cv2_to_imgmsg(image_result, encoding="bgr8")
+        #
+        #image_message = self.bridge.cv2_to_imgmsg(image_result, encoding="bgr8")
+        #
+        #self.image_pub.publish(image_message)
         
-        self.image_pub.publish(image_message)
+        cv2.imshow('Image', image_result)
+        t3 = time.time()
+
+        fps2 = 1 / (t3- t1)
+        print(f"FINAL FPS: {fps2:.3f}")
+        
+        # Press `q` to exit.
+        cv2.waitKey(1)
         
         
 class ImageViewer:
