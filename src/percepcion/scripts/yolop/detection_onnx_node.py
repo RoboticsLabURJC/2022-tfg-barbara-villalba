2fa50917c1ff01acefca236655bda43609eb093a
bb6698 <b.villalba.2019@alumnos.urjc.es>
2023-11-23 15:26:15 +0100

    Lines in image

--- src/percepcion/scripts/yolop/detection_onnx_node.py
+++ src/percepcion/scripts/yolop/detection_onnx_node.py
@@ -10,46 +10,23 @@ import onnxruntime as ort
 from sklearn.cluster import DBSCAN
 from numba import jit
 import matplotlib.pyplot as plt
+from scipy.ndimage import binary_dilation
 
 IMAGE_TOPIC = '/airsim_node/PX4/front_center_custom/Scene'
 
 ROUTE_MODEL = "/home/bb6/YOLOP/weights/yolop-320-320.onnx"
 
+MIN_VALUE_Y = 150
+MAX_VALUE_Y = 320
+
 @jit(nopython=True)
-def get_first_points_lanes(img_clustering):
-
-    first_left_lane_point = []
-    first_right_lane_point = []
-
-       
-    #--LEFT
-    for y in range(319,-1,-1):
-        for x in range(0,120):
-            if img_clustering[y,x] == 255:
-                first_left_lane_point.append([x,y])
-                break
-        if first_left_lane_point:
-            break
-
-    #--RIGHT
-    for y in range(319,-1,-1):
-        for x in range(319,120,-1):
-            if img_clustering[y,x] == 255:
-                first_right_lane_point.append([x,y])
-                break
-        if first_right_lane_point:
-            break
-
-    return first_left_lane_point,first_right_lane_point
-
-
 def calculate_values_xy(left_fit,right_fit):
         
-    left_lane_array = np.zeros((320, 320,3), dtype=np.uint8)
-    right_lane_array = np.zeros((320, 320,3), dtype=np.uint8)
+    left_lane = []
+    right_lane = []
 
     # Agregar valores en cada iteración de dos bucles for
-    for y in range(0,320):
+    for y in range(MIN_VALUE_Y,MAX_VALUE_Y):
     
         left_fitx= left_fit[0]*y**2 + left_fit[1]*y + left_fit[2]
 
@@ -57,20 +34,21 @@ def calculate_values_xy(left_fit,right_fit):
 
  
         if(left_fitx >= 0 and left_fitx < 320):
-           left_lane_array[y,int(left_fitx)]  = [0,255,0]
-
+           left_lane.append([y,int(left_fitx)])
+           
         if(left_fitx > 319):
-           left_lane_array[y,319]  = [0,255,0] 
+           left_lane.append([y,319])
 
         if(right_fitx >= 0 and right_fitx < 320):
-            right_lane_array[y,int(right_fitx)]  = [0,0,255]
+            right_lane.append([y,int(right_fitx)])
+            
 
         if(right_fitx > 319):
-            right_lane_array[y,319]  = [0,0,255]
-        
+            right_lane.append([y,319])
+
+    return left_lane,right_lane
 
 
-    return left_lane_array,right_lane_array
 
 
 class ImageSubscriber:
@@ -89,6 +67,10 @@ class ImageSubscriber:
         self.top_right    = [165, 150]
         self.vertices = np.array([[self.bottom_left_base,self.bottom_left, self.top_left, self.top_right, self.bottom_right,self.bottom_right_base]], dtype=np.int32)
         self.point_cluster = np.ndarray
+        self.kernel = np.array([[0,1,0], 
+                                [1,1,1], 
+                                [0, 1,0]]) 
+
         
 
     #--Resize image for more later yolop
@@ -158,9 +140,36 @@ class ImageSubscriber:
         left_fit = np.polyfit(lefty,leftx, 2)
         right_fit = np.polyfit(righty,rightx,2) 
 
-        img_left_lane,img_right_lane = calculate_values_xy(left_fit,right_fit)
+        """
+        valuesLineX = np.arange(150,320)
+
+        fitLineLeftY = np.polyval(left_fit, valuesLineX).astype(int)
+        fitLineRightY = np.polyval(right_fit, valuesLineX).astype(int)
+        
+        # Convertir los valores de fitLineY que sean mayores de 320 a 320
+        fitLineLeftY_capped = [y if y <= 319 else 319 for y in fitLineLeftY]
+
+        # Convertir los valores de fitLineY que sean mayores de 320 a 320
+        fitLineRightY_capped = [y if y <= 319 else 319 for y in fitLineRightY]
+
+        # Filtrar los valores de fitLineY_capped que sean mayores de 150 y menores de 320
+        fitLineLeftY_filtered = [(x, y) for x, y in zip(valuesLineX, fitLineLeftY_capped) if 0 <= y <= 319]
+
+        # Filtrar los valores de fitLineY_capped que sean mayores de 150 y menores de 320
+        fitLineRightY_filtered = [(x, y) for x, y in zip(valuesLineX, fitLineRightY_capped) if 0 <= y <= 319]
+
+        #print(len(fitLineLeftY_filtered))
+        #print( np.array(fitLineRightY_filtered))
+
+        """
+        
+
+        left_lane,right_lane = calculate_values_xy(left_fit,right_fit)
+
+        left_lane_points = np.array(left_lane)
+        right_lane_points = np.array(right_lane)
     
-        return img_left_lane,img_right_lane
+        return left_lane_points,right_lane_points
     
 
     def clustering(self,img):
@@ -170,7 +179,7 @@ class ImageSubscriber:
         dbscan = DBSCAN(eps=25, min_samples=1,metric="euclidean")
         dbscan.fit(points_lane)
         n_clusters_ = len(set(dbscan.labels_)) - (1 if -1 in dbscan.labels_ else 0)
-        #print("Clusters: " + str(n_clusters_))
+        print("Clusters: " + str(n_clusters_))
         self.point_cluster = []
 
         result = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
@@ -237,9 +246,9 @@ class ImageSubscriber:
         
         cvimage = cv2.resize(cvimage,(320,320),cv2.INTER_LINEAR)
         org_img = cvimage.copy()
-        cvimage[images[1] == 1] = [0, 0, 255]
+        #cvimage[images[1] == 1] = [0, 0, 255]
 
-        """
+
         mask = self.draw_region(images[1])
 
         masked_image = self.draw_region(cvimage)
@@ -249,16 +258,42 @@ class ImageSubscriber:
         cvimage[img_clustering == 255] = [0, 0, 255]
 
 
-        img_left_lanes,img_right_lanes= self.get_lane_line_indices(img_clustering)
+        left_lane_points,right_lane_points= self.get_lane_line_indices(img_clustering)
+
+        img_left_lane = np.zeros((320, 320), dtype=np.uint8)
+        img_right_lanes = np.zeros((320, 320), dtype=np.uint8)
         
+        img_left_lane[left_lane_points[:,0],left_lane_points[:,1]] = 1
+        img_right_lanes[right_lane_points[:,0],right_lane_points[:,1]] = 1
+
+        
+        img_left_lane_dilatada = binary_dilation(img_left_lane, structure=self.kernel)
+        img_left_lane_dilatada = (img_left_lane_dilatada).astype(np.uint8)
+
+        img_right_lane_dilatada = binary_dilation(img_right_lanes, structure=self.kernel)
+        img_right_lane_dilatada = (img_right_lane_dilatada).astype(np.uint8)
+
+
         img_lanes = np.zeros((320, 320,3), dtype=np.uint8)
-        img_lanes = img_left_lanes + img_right_lanes
-        return org_img,masked_image,cvimage,img_clustering,img_lanes
-        """
+        cvimage[img_left_lane_dilatada == 1] = [0,255,0]
+        cvimage[img_right_lane_dilatada == 1] = [0,0,255]
+
+        
 
 
 
+        #cv2.fillPoly(img_lanes, np.int32([left_lane_points]),[0,255,0],lineType=cv2.LINE_4)
+        #cv2.fillPoly(img_lanes, np.int32([right_lane_points]),[0,255,255],lineType=cv2.LINE_4)
+        
 
+
+
+        
+        #cv2.dilate(img_lanes, self.kernel, iterations=20)
+
+
+
+                
         t2 = time.time()
 
         fps = str (int(1/(t2-t1)))
@@ -274,26 +309,20 @@ class ImageSubscriber:
                 lineType=cv2.LINE_AA
             )
         
-        return cvimage
+        return cvimage,masked_image,img_lanes
         
 
     def callback(self, data):
 
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8") 
-        #image = np.float32(cv_image)
-        #image = cv2.convertScaleAbs(image,alpha=1.5)
-
-       
-
-        #image = np.uint8(image)
-        #cv_image = cv2.GaussianBlur(cv_image, (5, 5), 0)
-        org_img= self.infer_yolop(cv_image)
+     
+        org_img,masked_image,img_lanes= self.infer_yolop(cv_image)
 
         cv2.imshow('Image-Original-Network',org_img)
-        #cv2.imshow('Masked-Image',masked_image)
+        cv2.imshow('Masked-Image',masked_image)
         #cv2.imshow('Image',image_result)
         #cv2.imshow('img_cluster',img_cluster)
-        #cv2.imshow('Image-Lanes',img_lanes)
+        cv2.imshow('Image-Lanes',img_lanes)
        
         
         # Press `q` to exit.
