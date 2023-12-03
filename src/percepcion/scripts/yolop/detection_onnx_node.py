7bbdc84f184c5923a14947ecc27479237ec5f32b
bb6698 <b.villalba.2019@alumnos.urjc.es>
2023-12-03 21:44:35 +0100

    Erros correct

--- src/percepcion/scripts/yolop/detection_onnx_node.py
+++ src/percepcion/scripts/yolop/detection_onnx_node.py
@@ -2,7 +2,6 @@
 import torch
 import rospy
 from sensor_msgs.msg import Image,PointCloud2
-import sensor_msgs.point_cloud2
 from cv_bridge import CvBridge
 import numpy as np
 import cv2
@@ -17,12 +16,8 @@ from scipy.interpolate import interp1d
 from geometry_msgs.msg import PoseStamped, Twist
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest, CommandTOL, CommandTOLRequest
-from nav_msgs.msg import Odometry
-import warnings
-from numpy import RankWarning
-
-
-
+import sensor_msgs.point_cloud2
+from sklearn.linear_model import LinearRegression
 
 IMAGE_TOPIC = '/airsim_node/PX4/front_center_custom/Scene'
 
@@ -39,7 +34,6 @@ STATE_SUB = "mavros/state"
 MODE_SUB = "/commands/mode"
 LOCAL_VEL_PUB = "/mavros/setpoint_velocity/cmd_vel_unstamped"
 LIDAR = "/airsim_node/PX4/lidar/LidarCustom"
-
 MASS_CENTRE = "/yolop/detection_lane/mass_centre_lane"
 
 #--Services
@@ -49,13 +43,7 @@ TAKE_OFF_CLIENT = "/mavros/cmd/takeoff"
 
 OFFBOARD = "OFFBOARD"
 
-left_fit_global = np.ndarray([])
-right_fit_global = np.ndarray([])
-
-cx_global = 0.0
-cy_global = 0.0
-
-
+coefficients_global = np.array([])
 
 
 class ImageSubscriber:
@@ -67,10 +55,10 @@ class ImageSubscriber:
         self.list = []
         self.bottom_left_base = [0,320]
         self.bottom_right_base = [320,320]
-        self.bottom_left  = [0, 210]
-        self.bottom_right = [320,210]
-        self.top_left     = [80,175]
-        self.top_right    = [200, 175]
+        self.bottom_left  = [0, 230]
+        self.bottom_right = [320,230]
+        self.top_left     = [155,150]
+        self.top_right    = [165, 150]
         self.vertices = np.array([[self.bottom_left_base,self.bottom_left, self.top_left, self.top_right, self.bottom_right,self.bottom_right_base]], dtype=np.int32)
         self.point_cluster = np.ndarray
         self.kernel = np.array([[0,1,0], 
@@ -85,7 +73,6 @@ class ImageSubscriber:
 
         self.cx = 0
         self.cy = 0
-
 
  
     def resize_unscale(self,img, new_shape=(640, 640), color=114):
@@ -125,7 +112,7 @@ class ImageSubscriber:
         return canvas, r, dw, dh, new_unpad_w, new_unpad_h  # (dw,dh)
     
     def calculate_lineal_regression(self,points_cluster):
-
+        global coefficients_global
         """
         Calculate line thorugh lineal regression
 
@@ -137,12 +124,51 @@ class ImageSubscriber:
         """
 
         valuesX = np.arange(MIN_VALUE_X,MAX_VALUE_X) 
-        coefficients = np.polyfit(points_cluster[:,0],points_cluster[:,1],1)
+
+        try:
+            coefficients = np.polyfit(points_cluster[:,0],points_cluster[:,1],1)
+            coefficients_global = coefficients
+
+        except:
+            print("He fallado")
+            coefficients = coefficients_global
+        
         values_fy = np.polyval(coefficients,valuesX).astype(int)
         fitLine_filtered = [(x, y) for x, y in zip(valuesX, values_fy) if 0 <= y <= 319]
         line = np.array(fitLine_filtered)
 
         return line
+    
+
+    def filtrar_puntos_interp(self,img, reg_izquierda, reg_derecha, x_range):
+        """
+        Esta función devuelve los puntos que están entre las dos líneas de regresión usando interp1d.
+
+        Args:
+            puntos (np.array): Array de puntos a comprobar.
+            reg_izquierda (LinearRegression): Regresión lineal del lado izquierdo.
+            reg_derecha (LinearRegression): Regresión lineal del lado derecho.
+            x_range (np.array): Rango de valores x para la interpolación.
+
+        Returns:
+            np.array: Array de puntos que están entre las dos líneas.
+        """
+
+        puntos = np.column_stack(np.where(img > 0))
+        # Crear funciones de interpolación para las líneas de regresión
+        f_izq = interp1d(x_range, reg_izquierda.predict(x_range.reshape(-1, 1)), fill_value="extrapolate")
+        f_der = interp1d(x_range, reg_derecha.predict(x_range.reshape(-1, 1)), fill_value="extrapolate")
+
+   
+
+        y_values_f1 = f_izq(puntos[:, 0])
+        y_values_f2 = f_der(puntos[:, 0])
+        indices = np.where((y_values_f1 <= puntos[:, 1]) & (puntos[:, 1] <= y_values_f2))
+            
+            
+        points_between_lines = puntos[indices]
+
+        return points_between_lines
 
     def clustering(self,img):
 
@@ -157,30 +183,38 @@ class ImageSubscriber:
         """
         #--Convert image in points
         points_lane = np.column_stack(np.where(img > 0))
+        dbscan = DBSCAN(eps=20, min_samples=5,metric="euclidean")
+        clusters_izquierda = []
+        clusters_derecha = []
 
-        dbscan = DBSCAN(eps=15, min_samples=1,metric="euclidean")
         if points_lane.size > 0:
             dbscan.fit(points_lane)
-            n_clusters_ = len(set(dbscan.labels_)) - (1 if -1 in dbscan.labels_ else 0)
-            #print("Clusters: " + str(n_clusters_))
-            self.point_cluster = []
+            labels = dbscan.labels_
 
-            result = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
-
-            #--Save all clusters in list
-            for cluster in range(len(set(dbscan.labels_))):
+            # Ignore noise if present
+            clusters = set(labels)
+            if -1 in clusters:
+                clusters.remove(-1)
+            n_clusters_ = len(clusters)
+            print("Clusters: " + str(n_clusters_))
+          
+            for cluster in clusters:
+                points_cluster = points_lane[labels==cluster,:]
                 
-                self.point_cluster.append(points_lane[dbscan.labels_==cluster,:]) 
+                centroid = points_cluster.mean(axis=0)
+                if centroid[1] < img.shape[1] / 2:
+                    #print(f"El cluster {cluster} está a la izquierda.")
+                    clusters_izquierda.append(points_cluster)
+                else:
+                  
+                    clusters_derecha.append(points_cluster)
 
-            #--Concatenate all clusters in only numpy.ndarray
-            all_points = np.concatenate(self.point_cluster, axis=0)
 
-            result[all_points[:,0], all_points[:,1]] = 255
-
-            return result
+            return clusters_izquierda,clusters_derecha
         
         else:
-            return None
+            return None,None
+        
         
     
     def draw_region(self,img):
@@ -195,30 +229,23 @@ class ImageSubscriber:
     
     def calculate_mass_centre_lane(self,points_lane):
 
-        global cx_global,cy_global
+        if(points_lane.size > 0):
+            img = np.zeros((WIDTH, HEIGH), dtype=np.uint8)
+            img[points_lane[:,0],points_lane[:,1]] = 255
 
-        img = np.zeros((WIDTH, HEIGH), dtype=np.uint8)
-        img[points_lane[:,0],points_lane[:,1]] = 255
+            contornos, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(img, contornos, -1, 255, thickness=cv2.FILLED)
 
-        contornos, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(img, contornos, -1, 255, thickness=cv2.FILLED)
+            momentos = cv2.moments(img)
 
-        momentos = cv2.moments(img)
-
-        # Calcular el centro de masa
-        try:
+            # Calcular el centro de masa
             cx = int(momentos['m10'] / momentos['m00'])
             cy = int(momentos['m01'] / momentos['m00'])
 
-            cx_global = cx
-            cy_global = cy
-
-        except BaseException as e:
-            cx = cx_global
-            cy = cy_global
-
-
-        return cx,cy
+            return cx,cy
+        
+        else:
+            return 160,160
     
     def dilate_lines(self,left_line_points,right_line_points):
 
@@ -323,133 +350,55 @@ class ImageSubscriber:
         return images
 
         
-    def calculate_margins_points(self,img,cvimage,img_da_seg):
-        global left_fit_global,right_fit_global
-          # Encuentra las coordenadas x e y de todos los distintos de cero
-        # (es decir, blancos) píxeles en el marco.
-        nonzero = img.nonzero()
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-            
-        # Almacene los índices de píxeles para las líneas de los carriles izquierdo y derecho
-        left_lane_inds = []
-        right_lane_inds = []
-            
-       # Identificar los píxeles distintos de cero (es decir, blancos) en x e y dentro de la ventana
-        good_left_inds = ((nonzeroy >= 0) & (nonzeroy <= 319) & (nonzerox >= 0) & (nonzerox < 160)).nonzero()[0]
-        #print(((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero())
-        #print(((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0])
+    def calculate_margins_points(self,left_clusters,right_clusters,cvimage,img_da_seg):
         
-        good_right_inds = ((nonzeroy >= 0) & (nonzeroy <= 319) & (nonzerox > 160) & (nonzerox <= 319)).nonzero()[0]
-
-        #print(good_left_inds,good_right_inds)
-                                                            
-        # Agregar estos índices a las listas.
-        left_lane_inds.append(good_left_inds)
-        right_lane_inds.append(good_right_inds)
             
-       
-        # Concatenar los arrays de índices
-        left_lane_inds = np.concatenate(left_lane_inds)
-        right_lane_inds = np.concatenate(right_lane_inds)
+            if(left_clusters and right_clusters):
+                left = np.concatenate(left_clusters,axis=0)
+                right = np.concatenate(right_clusters,axis=0)
 
-    
-        # Extrae las coordenadas de píxeles para las líneas de los carriles izquierdo y derecho
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds] 
-        rightx = nonzerox[right_lane_inds] 
-        righty = nonzeroy[right_lane_inds]
+                cvimage[left[:,0],left[:,1]] = [0,0,255]
+                cvimage[right[:,0],right[:,1]] = [0,255,0]
 
-        valuesLineX = np.arange(170,320)
+                points_line_right =  self.calculate_lineal_regression(right)
+                points_line_left = self.calculate_lineal_regression(left)
+
+                img_line_left,img_line_right = self.dilate_lines(points_line_left,points_line_right)
+
+                cvimage[img_line_left == 1] = [255,255,255]
+                cvimage[img_line_right == 1] = [255,255,255]
+
+                points_beetween_lines = self.interpolate_lines(img_da_seg,points_line_left,points_line_right)
+
+
+                cvimage[points_beetween_lines[:,0],points_beetween_lines[:,1]] = [255,0,0]
+
+                self.cx,self.cy = self.calculate_mass_centre_lane(points_beetween_lines)
+
+                cv2.circle(cvimage, (self.cx,self.cy), radius=10, color=(0, 0, 0),thickness=-1)
         
-        #print("Left: " + str(leftx.size) + " , " + str(lefty.size))
-        #print("Right: " + str(rightx.size) + " , " + str(righty.size))
-
-        try:
-            left_fit = np.polyfit(lefty,leftx, 1)
-            right_fit = np.polyfit(righty,rightx,1) 
-            left_fit_global = left_fit
-            right_fit_global = right_fit
-
-        except BaseException as e:
-           print("Entre")
-           left_fit =  left_fit_global
-           right_fit = right_fit_global
+            return cvimage
     
-        #print(left_fit,right_fit)
 
-        fitLineLeftY = np.polyval(left_fit, valuesLineX).astype(int)
-        fitLineRightY = np.polyval(right_fit, valuesLineX).astype(int)
-        
-       
-
-        # Filtrar los valores de fitLineY_capped que sean mayores de 150 y menores de 320
-        fitLineLeftY_filtered = [(x, y) for x, y in zip(valuesLineX, fitLineLeftY) if 0 <= y <= 319]
-
-        # Filtrar los valores de fitLineY_capped que sean mayores de 150 y menores de 320
-        fitLineRightY_filtered = [(x, y) for x, y in zip(valuesLineX, fitLineRightY) if 0 <= y <= 319]
-
-        left_lane_points = np.array(fitLineLeftY_filtered)
-        right_lane_points = np.array(fitLineRightY_filtered)
-
-        img_left_lane_dilatada,img_right_lane_dilatada = self.dilate_lines(left_lane_points,right_lane_points)
-
-        cvimage[img_left_lane_dilatada == 1] = [255,255,255]
-        cvimage[img_right_lane_dilatada == 1] = [255,255,255]
-
-        points_beetween_lines = self.interpolate_lines(img_da_seg,left_lane_points,right_lane_points)
-
-
-        cvimage[points_beetween_lines[:,0],points_beetween_lines[:,1]] = [255,0,0]
-
-        self.cx,self.cy = self.calculate_mass_centre_lane(points_beetween_lines)
-
-        cv2.circle(cvimage, (self.cx,self.cy), radius=10, color=(0, 0, 0),thickness=-1)
-       
-
-
-        return cvimage
-    
     def calculate_fps(self):
         fps = 1/(time.time() - self.t1)
         return fps
     
     def callback(self, data):
 
-        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        cv2.GaussianBlur(cv_image,(3,3),0) 
-
-        """
-        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        hsv = np.array(hsv, dtype = np.float64)
-        hsv[:,:,1] = hsv[:,:,1]*1.25 # scale pixel values up for channel 1
-        hsv[:,:,1][hsv[:,:,1]>255]  = 255
-        hsv[:,:,2] = hsv[:,:,2]*1.35 # scale pixel values up for channel 2
-        hsv[:,:,2][hsv[:,:,2]>255]  = 255
-        hsv = np.array(hsv, dtype = np.uint8)
-        img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        """
-
-
+        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8") 
      
         images_yolop = self.infer_yolop(cv_image)
-        #images_yolop2 = self.infer_yolop(cv_image)
-        mask_cvimage = self.draw_region(cv2.resize(cv_image,(WIDTH,HEIGH),cv2.INTER_LINEAR))
         mask = self.draw_region(images_yolop[1])
+        left_clusters,right_clusters = self.clustering(mask)
 
+        if left_clusters and right_clusters is None:
+            return
+        out_img = self.calculate_margins_points(left_clusters,right_clusters,cv2.resize(cv_image,(WIDTH,HEIGH),cv2.INTER_LINEAR),images_yolop[0])
+
+
+        #out_img = self.calculate_margins_points(dict_clusters,cv2.resize(cv_image,(WIDTH,HEIGH),cv2.INTER_LINEAR),images_yolop[0])
         
-        img_clustering= self.clustering(mask)
-
-        if img_clustering is None:
-            return 
-
-
-        cv_image = cv2.resize(cv_image,(WIDTH,HEIGH),cv2.INTER_LINEAR)
-        #img[images_yolop[1] == 1] = [0,0,255]
-        cvimage = cv2.resize(cv_image,(WIDTH,HEIGH),cv2.INTER_LINEAR)
-        #cvimage[images_yolop2[1] == 1] = [0,0,255]
-        out_img = self.calculate_margins_points(img_clustering,cv_image,images_yolop[0])
-                   
         self.counter_time =  self.counter_time + (time.time() - self.t1)
 
         if(not self.isfirst):
@@ -478,8 +427,7 @@ class ImageSubscriber:
 
 
         cv2.imshow('Image',out_img)
-        cv2.imshow('Sin Brillo',cvimage)
-        cv2.imshow('Mask',mask_cvimage)
+
     
         # Press `q` to exit.
         cv2.waitKey(3)
@@ -499,9 +447,9 @@ class LaneFollow():
         self.error = 0
         self.error_height = 0
         self.KP_w = 0.03
-        self.KD_w = 0.005
+        self.KD_w = 0.007
         self.KP_v = 0.005
-        self.KD_v = 0.07
+        self.KD_v = 0.2
     
         self.velocity = Twist()
 
@@ -517,7 +465,7 @@ class LaneFollow():
 
     
     def height_velocity_controller(self):
-        error = round((2.5 - self.distance_z),2)
+        error = round((2.88 - self.distance_z),2)
         derr = error - self.prev_error_height
 
         self.velocity.linear.z = (self.KP_v * error) + (self.KD_v * derr)
@@ -525,7 +473,7 @@ class LaneFollow():
     def velocity_controller(self,cx):
         self.error = WIDTH/2 - cx
         derr = self.error - self.prev_error
-        #print(self.error)
+        print(self.error)
 
         self.velocity.angular.z = (self.KP_w * self.error) + (self.KD_w * derr)
 
@@ -547,17 +495,20 @@ if __name__ == '__main__':
 
     while (not rospy.is_shutdown()):
         
+       
+        print(lane_follow.distance_z)
+        
+        
         if (lane_follow.current_state.mode != OFFBOARD and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
             if (lane_follow.set_mode_client.call(set_mode).mode_sent is True):
                 rospy.loginfo("OFFBOARD enabled")
 
-        print(lane_follow.distance_z)
+        
         lane_follow.velocity_controller(image_viewer.cx)
         lane_follow.height_velocity_controller()
         lane_follow.prev_error_height = lane_follow.error_height
         lane_follow.prev_error = lane_follow.error_height
         lane_follow.local_raw_pub.publish(lane_follow.velocity)
+        
 
         rate.sleep()
-
-    #image_viewer.start()
